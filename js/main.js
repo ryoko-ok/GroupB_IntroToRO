7490f69d8963910cf7150ba2b1e6abe872ed8afc
Oka <r_oka@ats-hr420-dm03.fc.ca>
2020-01-10 14:37:26 -0500

    make first page with button

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// this is a self-invoking anonymous function
+// also called an IIFE
+//also called a module
+
+(() => {
+  // print("some message");
+
+  console.log("some message - from JavaScript");
+
+  // find the button element on the page by its ID with JavaScript
+  let theButton = document.querySelector("#myButton");
+      theHeading = document.querySelector('h1');
+      svgImage = document.querySelector("#svgGraphic");
+
+  // this function changes the heading text
+  function changeText() {
+    theHeading.textContent = "Hello there from the console";
+  }
+
+  // log the id of the svg to the console when a user hovers on it
+  function logSVG() {
+    console.log(this.id);
+  }
+
+  theButton.addEventListener("click", changeText);
+  svgImage.addEventListener("mouseover", logSVG);
+  //document.querySelector('h1').textContent = "Hello there from the console";
+})();
